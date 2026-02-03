ee7ce38fd1447daa1759bd2d9f59cf9f82b92d8b
Carlos Marquina <carlosscripto@gmail.com>
2026-02-03 04:38:05 +0100

    Add test to verify calculateMedian does not mutate input array

--- prep/median.test.js
+++ prep/median.test.js
@@ -5,3 +5,10 @@ test("calculates the median of a list of odd length", () => {
 
   expect(currentOutput).toEqual(targetOutput);
 });
+
+test("doesn't modify the input", () => {
+  const list = [1, 2, 3];
+  calculateMedian(list);
+
+  expect(list).toEqual([1, 2, 3]); // Note that the toEqual matcher checks the values inside arrays when comparing them - it doesn't use `===` on the arrays, we know that would always evaluate to false.
+});
