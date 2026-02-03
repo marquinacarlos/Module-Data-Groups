6aa4a71d0d30de78925ccace4e52dfe53c20e94a
Carlos Marquina <carlosscripto@gmail.com>
2026-02-03 04:42:45 +0100

    Fix calculateMedian to use index access instead of splice

--- prep/median.js
+++ prep/median.js
@@ -2,7 +2,8 @@ import { calculateMean } from "./mean";
 
 function calculateMedian(list) {
   const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
+  // const median = list.splice(middleIndex, 1)[0];
+  const median = list[middleIndex];
 
   return median;
 }
