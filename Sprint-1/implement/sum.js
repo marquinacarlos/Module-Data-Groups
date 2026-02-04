c938b83484af71e7f2fa57424f6b49177bacdbd9
Carlos Marquina <carlosscripto@gmail.com>
2026-02-04 04:32:42 +0100

    Add type checking to sum function to filter non-numeric values

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,10 +1,12 @@
 function sum(elements = []) {
 	if(elements.length === 0) return 0;
 	const initialValue = 0;
-	const sumWithInitial = elements.reduce(
-		(accumulator, currentValue) => accumulator + currentValue,
-		initialValue
-	);
+	const callback = (accumulator, currentValue) => {
+		return typeof currentValue === "number" && !isNaN(currentValue)
+			? accumulator + currentValue
+			: accumulator
+	}
+	const sumWithInitial = elements.reduce(callback, initialValue);
 	return sumWithInitial;
 }
 
