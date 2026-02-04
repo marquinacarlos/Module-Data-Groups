1c55eecd3bb0879213ac5422961da24e71a6e8f9
Carlos Marquina <carlosscripto@gmail.com>
2026-02-04 04:44:52 +0100

    Implement dedupe function using Set to remove duplicates

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,8 @@
-function dedupe() {}
+function dedupe(arr = []) {
+	if(arr.length === 0) return arr;
+	const newSet = new Set(array)
+	const newArr = [...newSet];
+	return newArr;
+}
+
+module.exports = dedupe;
