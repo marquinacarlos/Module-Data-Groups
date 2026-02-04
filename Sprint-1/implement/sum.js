4bece27ae8fb0d1add2f6287ff1110f6ff9450df
Carlos Marquina <carlosscripto@gmail.com>
2026-02-04 04:34:25 +0100

    Refactor sum function to use inline reduce callback

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,13 +1,10 @@
 function sum(elements = []) {
 	if(elements.length === 0) return 0;
-	const initialValue = 0;
-	const callback = (accumulator, currentValue) => {
+	return elements.reduce((accumulator, currentValue) => {
 		return typeof currentValue === "number" && !isNaN(currentValue)
 			? accumulator + currentValue
 			: accumulator
-	}
-	const sumWithInitial = elements.reduce(callback, initialValue);
-	return sumWithInitial;
+	}, 0);
 }
 
 module.exports = sum;
