553f791b4ad4d8e772bf14070d60fa1c729e3260
Carlos Marquina <carlosscripto@gmail.com>
2026-02-04 04:16:23 +0100

    Implement sum function with reduce and empty array handling

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
-function sum(elements) {
+function sum(elements = []) {
+	if(elements.length === 0) return 0;
+	const initialValue = 0;
+	const sumWithInitial = elements.reduce(
+		(accumulator, currentValue) => accumulator + currentValue,
+		initialValue
+	);
+	return sumWithInitial;
 }
 
 module.exports = sum;
