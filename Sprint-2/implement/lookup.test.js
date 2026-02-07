fffaccca86c75014caf1def3b4e2a2da59cd4086
Carlos Marquina <carlosscripto@gmail.com>
2026-02-07 01:49:54 +0100

    Add test for createLookup with country currency code pairs

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,6 +1,10 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+test("creates a country currency code lookup for multiple codes", () => {
+  const input = [["US", "USD"], ["CA", "CAD"]];
+  const result = createLookup(input);
+  expect(result).toEqual({ US: "USD", CA: "CAD" });
+});
 
 /*
 
